293349d84f244b672a228b8d272a8cdeafeabbc8
Ashwani Patel <ashwanipatel29998@gmail.com>
2023-09-28 17:41:57 +0530

    gmail clone

---
+++ src/components/SideBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import{  Drawer,styled } from '@mui/material';
+
+
+const SideBar = () => {
+  return (
+   <Drawer
+    anchor='left'
+    open={true}
+    hideBackdrop={true}
+   >
+      hello
+   </Drawer>
+  )
+}
+
+export default SideBar
---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import SideBar from '../components/SideBar'
+
+const Main = () => {
+  return (
+    <div>
+    <Header></Header>
+    <SideBar></SideBar>
+    <div>all mail content</div>
+    </div>
+  )
+}
+
+export default Main
